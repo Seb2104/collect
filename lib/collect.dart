f45bfa1d81a4e97863683b1e52e70480af317a8d
Seb2104 <66326679+Seb2104@users.noreply.github.com>
2026-02-03 00:01:33 +0200

    #29 - Added notifs

--- lib/collect.dart
+++ lib/collect.dart
@@ -10,7 +10,6 @@ import 'package:flutter/services.dart';
 import 'package:unicode/unicode.dart' as unicode;
 
 part 'constants/bases.dart';
-part 'presentation/styled_box.dart';
 part 'constants/colours.dart';
 part 'constants/moment.dart';
 part 'constants/strings.dart';
@@ -18,6 +17,7 @@ part 'constants/tab_view.dart';
 part 'datatypes/colour.dart';
 part 'datatypes/moment.dart';
 part 'datatypes/period.dart';
+part 'extensions/build_context.dart';
 part 'extensions/double.dart';
 part 'extensions/int.dart';
 part 'extensions/num.dart';
@@ -26,8 +26,9 @@ part 'presentation/action_icon.dart';
 part 'presentation/app_theme.dart';
 part 'presentation/inset_box_shadow.dart';
 part 'presentation/menu.dart';
+part 'presentation/styled_box.dart';
 part 'presentation/tab_view.dart';
 part 'presentation/word.dart';
+part 'utils/notifications.dart';
 part 'utils/radix.dart';
-
 part 'utils/strings.dart';
---
+++ lib/extensions/build_context.dart
@@ -0,0 +1,30 @@
+part of '../collect.dart';
+
+extension NotificationExtension on BuildContext {
+  void notify(String message, {Duration? duration}) {
+    NotificationManager().show(
+      context: this,
+      message: message,
+      type: NotificationType.info,
+      duration: duration,
+    );
+  }
+
+  void warn(String message, {Duration? duration}) {
+    NotificationManager().show(
+      context: this,
+      message: message,
+      type: NotificationType.warning,
+      duration: duration,
+    );
+  }
+
+  void fail(String message, {Duration? duration}) {
+    NotificationManager().show(
+      context: this,
+      message: message,
+      type: NotificationType.error,
+      duration: duration,
+    );
+  }
+}
