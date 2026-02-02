4cc75ca014da6ede863676331e85d2ce78f936cc
Seb2104 <66326679+Seb2104@users.noreply.github.com>
2026-02-02 14:24:26 +0200

    #9 - jobneh

--- lib/collect.dart
+++ lib/collect.dart
@@ -8,7 +8,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:unicode/unicode.dart' as unicode;
 
-part 'widgets/box.dart';
+part 'widgets/box.dart' ;
 part 'datatypes/colour/bases.dart';
 part 'datatypes/colour/colour.dart';
 part 'datatypes/colour/colours.dart';
