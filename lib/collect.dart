005ffbe5a7d6c8c5b9ef98853d9ea2cca0af3ec7
Seb2104 <66326679+Seb2104@users.noreply.github.com>
2026-02-03 20:12:04 +0200

    #49 - FUCKNG HELL:

--- lib/collect.dart
+++ lib/collect.dart
@@ -2,6 +2,7 @@ library;
 
 import 'dart:math' as math;
 import 'dart:ui';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide FilterCallback, SearchCallback;
 import 'package:flutter/material.dart';
@@ -9,60 +10,31 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:unicode/unicode.dart' as unicode;
 
-part 'icons/icon_collection.dart';
-
 part 'constants/bases.dart';
-
 part 'constants/colours.dart';
-
 part 'constants/moment.dart';
-
 part 'constants/strings.dart';
-
 part 'constants/tab_view.dart';
-
 part 'datatypes/colour.dart';
-
 part 'datatypes/moment.dart';
-
 part 'datatypes/period.dart';
-
 part 'extensions/build_context.dart';
-
 part 'extensions/double.dart';
-
 part 'extensions/int.dart';
-
 part 'extensions/list.dart';
-
 part 'extensions/num.dart';
-
 part 'extensions/string.dart';
-
 part 'extensions/widget.dart';
-
 part 'presentation/action_icon.dart';
-
 part 'presentation/app_theme.dart';
-
 part 'presentation/box.dart';
-
 part 'presentation/fonts.dart';
-
 part 'presentation/hex_viewer.dart';
-
 part 'presentation/hover_detector.dart';
-
 part 'presentation/menu.dart';
-
 part 'presentation/rounded_checkmark.dart';
-
 part 'presentation/tab_view.dart';
-
 part 'presentation/word.dart';
-
 part 'utils/notifications.dart';
-
 part 'utils/radix.dart';
-
 part 'utils/strings.dart';
