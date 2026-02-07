e268b3cfc1a43efef24cf7a4f83d5ff9e888e772
Seb2104 <66326679+Seb2104@users.noreply.github.com>
2026-02-07 18:00:12 +0200

    #58 - POOP

--- testapp/lib/main.dart
+++ testapp/lib/main.dart
@@ -1,20 +1,32 @@
+import 'package:collect/api/presentation/colour_picker.dart';
 import 'package:collect/collect.dart';
 import 'package:flutter/material.dart';
 
+Colour colour = Colours.black;
+
 void main() {
-  runApp(const MainApp());
+  runApp(const Main());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Main extends StatelessWidget {
+  const Main({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.light(),
       home: Scaffold(
+        backgroundColor: AppTheme.background(context),
         body: Center(
-          child: Text('Hello World!'),
+          child: Container(
+            child: ColourPicker.wheel(
+              pickerColor: colour,
+              onColorChanged: (value) {
+                colour = Colour.fromColor(value);
+                print(colour);
+              },
+            ),
+          ),
         ),
       ),
     );
