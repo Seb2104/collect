881840061eaef47358794aef90ce59c8f77b4507
Seb2104 <66326679+Seb2104@users.noreply.github.com>
2026-02-09 02:31:49 +0200

    #94 - YAY

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,52 +1,76 @@
 import 'package:collect/collect.dart';
+import 'package:flutter/material.dart';
 
-Future<void> main() async {
-  print(await Fonts().loadAllFonts());
+Colour colour = Colours.white;
+
+void main() {
+  runApp(Main());
 }
 
-// import 'package:collect/collect.dart';
-// import 'package:example/icons.dart';
-// import 'package:flutter/material.dart';
-//
-// Colour colour = Colours.white;
-//
-// void main() {
-//   runApp(Main());
-// }
-//
-// class Main extends StatelessWidget {
-//   const Main({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       theme: AppTheme.light(),
-//       home: Scaffold(
-//         backgroundColor: AppTheme.background(context),
-//         body: SizedBox(
-//           height: MediaQuery
-//               .of(context)
-//               .size
-//               .height * 1,
-//           width: MediaQuery
-//               .of(context)
-//               .size
-//               .width * 1,
-//           child: Center(
-//             child: Container(
-//               // child: ColourPicker.wheel(
-//               //   height: 250,
-//               //   width: 800,
-//               //   currentColour: colour,
-//               //   onColorChanged: (value) {
-//               //     print(value);
-//               //   },
-//               // ),
-//               child: Icon(MyFlutterApp.format_colour_fill),
-//             ),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.light(),
+      home: Scaffold(
+        backgroundColor: AppTheme.background(context),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height * 1,
+          width: MediaQuery.of(context).size.width * 1,
+          child: GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              mainAxisExtent: 250,
+              crossAxisCount: 2,
+              crossAxisSpacing: 50,
+              mainAxisSpacing: 50,
+            ),
+            children: [
+              Container(
+                color: Colours.blueGrey,
+                height: 250,
+                width: 800,
+                child: ColourPicker.wheel(
+                  height: 200,
+                  width: 800,
+                  currentColour: colour,
+                  onColorChanged: (value) {
+                    print(value);
+                  },
+                ),
+              ),
+              Container(
+                color: Colours.blueGrey,
+                height: 250,
+                width: 800,
+                child: ColourPicker.slides(
+                  currentColour: colour,
+                  onColorChanged: (value) {},
+                ),
+              ),
+              Container(
+                color: Colours.blueGrey,
+                height: 250,
+                width: 800,
+                child: ColourPicker.ring(
+                  currentColour: colour,
+                  onColorChanged: (value) {},
+                ),
+              ),
+              Container(
+                color: Colours.blueGrey,
+                height: 100,
+                width: 800,
+                child: ColourPicker.square(
+                  currentColour: colour,
+                  onColorChanged: (value) {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
